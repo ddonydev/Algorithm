830c83a9d64861a9ebebf752b3f2a35ce50d6de3
thdmsdl97 <thdmsdl97@gmail.com>
2022-11-06 00:16:34 +0900

    프로그래머스 알고리즘 입문

---
+++ Beginners1.java
@@ -0,0 +1,12 @@
+public class Beginners1 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+
+            int answer = num1 * num2;
+
+            return answer;
+        }
+    }
+
+}
---
+++ Beginners6.java
@@ -0,0 +1,10 @@
+public class Beginners6 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+
+            return num1 - num2;
+        }
+    }
+
+}
