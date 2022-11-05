374ce04da1b347ff276161fd9af9ad30b2347106
thdmsdl97 <thdmsdl97@gmail.com>
2022-11-06 01:11:25 +0900

    프로그래머스 알고리즘 입문

---
+++ Beginners10.java
@@ -0,0 +1,15 @@
+public class Beginners10 {
+
+    class Solution {
+        public double solution(int[] numbers) {
+            double answer = 0;
+
+            for(int i : numbers){
+                answer += i;
+            }
+
+            return answer/numbers.length;
+        }
+    }
+
+}
