cca2569434227b2f312f9361df6abd323fbb54ec
thdmsdl97 <thdmsdl97@gmail.com>
2022-11-08 18:52:20 +0900

    프로그래머스 알고리즘 입문

---
+++ Beginners14.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Beginners14 {
+
+    class Solution {
+        public int solution(int[] array) {
+            int answer = 0;
+
+            Arrays.sort(array);
+
+            return array[array.length/2];
+
+
+        }
+    }
+
+}
