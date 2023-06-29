81ad4e9504fc7084affb77130007e0cbf2eb9afa
soeun <ddonydev@gmail.com>
2023-06-29 18:15:38 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.28 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String my_string) {
+        my_string = my_string.replaceAll("\\s+", " ");
+        my_string = my_string.trim();
+
+        String[] answer = my_string.split(" ");
+        
+        return answer;
+    }
+}
