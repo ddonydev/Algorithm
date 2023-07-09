883de517f227bd5fa278ae64c7ec2680b44e9fe9
soeun <ddonydev@gmail.com>
2023-07-09 22:07:36 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
        
         for(int i = 0; i < arr.length; i++){
-            if((arr[i] == 1) && (i > idx) || (arr[i] == 1) && (i == idx)){
+            if((arr[i] == 1) && (i >= idx)){
                 return i;
             }else{
                 answer = -1;
