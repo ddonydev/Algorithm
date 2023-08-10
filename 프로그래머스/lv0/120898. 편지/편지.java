154083a8069fdc94c70ce874539e8fd842941c1f
soeun <ddonydev@gmail.com>
2023-08-10 12:23:00 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int letter = message.length();
+        
+        answer = 2 * letter;
+        
+        return answer;
+    }
+}
