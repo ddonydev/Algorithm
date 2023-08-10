d90aedbd2db83d7a0e418cdbe83e1b4e4a95b6a1
soeun <ddonydev@gmail.com>
2023-08-10 12:26:45 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sqrt = (int)Math.sqrt(n);
+        
+       if(n % sqrt == 0){
+           answer = 1;
+       }else{
+           answer = 2;
+       }
+        
+        return answer;
+    }
+}
