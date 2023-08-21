97b2b0bbc6c2e7d52e5ad884c81b7308f7840da4
soeun <ddonydev@gmail.com>
2023-08-21 15:29:08 +0900

    [unrated] Title: 세 개의 구분자, Time: 73.23 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public String[] solution(String myStr) {
+        String[] str = myStr.split("[abc]");
+
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < str.length; i++) {
+            if(str[i] == null || str[i].equals("")){
+                continue;
+            }else{
+                list.add(str[i]);
+            }
+        }
+        String[] answer = new String[list.size()];
+
+        if(list.size() > 0){
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+        }else{
+            answer = new String[1];
+            answer[0] = "EMPTY";
+        }
+        return answer;
+    }
+}
