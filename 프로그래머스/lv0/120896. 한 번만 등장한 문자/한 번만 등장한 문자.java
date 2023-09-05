9209ae26420a0bd48bee176fd506df6f9ae446ba
soeun <ddonydev@gmail.com>
2023-09-05 12:19:14 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.49 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split("");
+        
+        Arrays.sort(str);
+
+        String answer = "";
+        for (int i = 0; i < str.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < str.length; j++) {
+                if(str[i].equals(str[j])){
+                    cnt++;
+                }
+            }
+            if(cnt == 1){
+                answer += str[i];
+            }
+        }
+        return answer;
+    }
+}
