01b32548f1abb69067daec5958a2fb59aef62efc
soeun <ddonydev@gmail.com>
2023-10-08 12:31:55 +0900

    [level 0] Title: 등수 매기기, Time: 0.27 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[][] score) {
+        Double[] tmp = new Double[score.length];
+
+        for (int i = 0; i < score.length; i++) {
+            tmp[i] = (score[i][0] + score[i][1]) / 2.0;
+        }
+
+        Arrays.sort(tmp, Collections.reverseOrder());
+
+        Map<Double, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < tmp.length; i++) {
+            if (!map.containsKey(tmp[i])) {
+                map.put(tmp[i], i + 1);
+            }
+        }
+
+        int[] answer = new int[score.length];
+
+        for (int i = 0; i < tmp.length; i++) {
+            answer[i] = map.get((score[i][0] + score[i][1]) / 2.0);
+        }
+        return answer;
+    }
+}
