96a12bfb1d51a4d12eccfca8ffefea38cac73074
soeun <ddonydev@gmail.com>
2023-10-26 15:12:36 +0900

    [level 1] Title: 시저 암호, Time: 39.73 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,20 +2,25 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         for (int i = 0; i < s.length(); i++) {
-            int sum = s.charAt(i) + n;
-            if (s.charAt(i) >= 65 && s.charAt(i) <= 90 && sum > 90) {
-                int sub = sum - 26;
-                answer += (char) sub;
-            } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122 && sum > 122) {
-                int sub = sum - 26;
-                answer += (char) sub;
-            } else if (s.charAt(i) == 32) {
-                answer += " ";
-            }else{
-                answer += (char) sum;
+            int str = s.charAt(i);
+            int sum = str + n;
+            if (str >= 65 && str <= 90 && sum > 90) {
+                sum -= 26;
             }
 
-        }
+            if (str >= 97 && str <= 122 && sum > 122) {
+                sum -= 26;
+            }
+
+            if (str == 32) {
+                answer += " ";
+                continue;
+            }
+
+            answer += (char) sum;
+
+        }   
+        
         return answer;
     }
 }
