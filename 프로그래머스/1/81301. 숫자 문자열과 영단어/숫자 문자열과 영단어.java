a8b23202e6065306d88e3e7a58d3c5c69487d7c1
soeun <ddonydev@gmail.com>
2023-10-29 17:17:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.32 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,24 +3,8 @@ class Solution {
     public int solution(String s) {
         String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
-        Map<String, String> map = new LinkedHashMap<>();
-
         for (int i = 0; i < num.length; i++) {
-            map.put(num[i], String.valueOf(i));
-        }
-
-        String str = "";
-        String tmp = s;
-
-        for (int i = 0; i < tmp.length(); i++) {
-            if (Character.isDigit(tmp.charAt(i))) {
-                continue;
-            }
-            str += tmp.charAt(i);
-            if (map.containsKey(str)) {
-                s = s.replace(str, map.get(str));
-                str = "";
-            }
+            s = s.replaceAll(num[i], String.valueOf(i));
         }
 
         return Integer.parseInt(s);
