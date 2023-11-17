e3c59571b3fa6a96248610eb4a662357aff504a7
soeun <ddonydev@gmail.com>
2023-11-18 00:15:15 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,16 +2,9 @@ class Solution {
     public int solution(int a, int b, int n) {
         int empty = 0;
 
-        while (n > 1 && n >= a) {
-            if (n % a == 0) {
-                n = (n / a) * b;
-                empty += n;
-            }else{
-                int tmp = n % a;
-                n = (n / a) * b;
-                empty += n;
-                n += tmp;
-            }
+        while (n >= a) {
+            n -= a - b;
+            empty += b;
         }
         return empty;
     }
