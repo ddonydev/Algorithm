97102163cd7c2e25b37ce3619557fe7dae2b5e11
soeun <ddonydev@gmail.com>
2023-11-29 01:28:30 +0900

    [level 1] Title: 소수 만들기, Time: 0.28 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    list.add(sum);
+                }
+            }
+        }
+
+        int max = Collections.max(list);
+        System.out.println(max);
+
+        Set<Integer> set = new HashSet<>();
+
+        int cnt = 0;
+        for (int i = 2; i <= max; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    cnt++;
+                }
+            }
+
+            if (cnt == 1) {
+                set.add(i);
+            }
+                cnt = 0;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (set.contains(list.get(i))) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
