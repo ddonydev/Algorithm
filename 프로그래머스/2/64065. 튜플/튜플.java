d1b8b3ed461652acad9e76910fa9eeef677fb718
soeun <ddonydev@gmail.com>
2023-12-13 19:52:54 +0900

    [level 2] Title: 튜플, Time: 0.80 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        String[] str = s.split("\\{|,|\\}");
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].matches("[0-9]+")) {
+                map.put(Integer.valueOf(str[i]), map.getOrDefault(Integer.valueOf(str[i]), 0) + 1);
+            }
+        }
+        List<Integer> keySetList = new ArrayList<>(map.keySet());
+        Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
+
+        int[] answer = new int[map.size()];
+        int index = 0;
+        for(Integer key : keySetList) {
+            answer[index] = key;
+            index++;
+        }
+        return answer;
+    }
+}
