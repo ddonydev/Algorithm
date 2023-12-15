0fdc2923c0c461cb12aa55484bfff3448dfcbcc8
soeun <ddonydev@gmail.com>
2023-12-15 18:52:30 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,15 +1,7 @@
-import java.util.*;
 class Solution {
     private static boolean[] visited;
     private static int answer;
     public int solution(int k, int[][] dungeons) {
-        Arrays.sort(dungeons, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[0]-o1[0];
-            }
-        });
-
         visited = new boolean[dungeons.length];
 
         backTracking(0, k, dungeons);
