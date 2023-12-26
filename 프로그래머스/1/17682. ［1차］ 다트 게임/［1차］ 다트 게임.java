0a9ee1633f41826b04e49c5c3c16a2ba44ece398
soeun <ddonydev@gmail.com>
2023-12-27 01:27:00 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.32 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -2,44 +2,44 @@ import java.util.*;
 class Solution {
     public int solution(String dartResult) {
         String[] dartStr = dartResult.split("");
-        
+
         Map<String, Integer> map = new HashMap<>();
         map.put("S", 1);
         map.put("D", 2);
         map.put("T", 3);
 
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         for(int i = 0; i < dartStr.length; i++){
             if(dartStr[i].matches("[0-9]")){
                 if(dartStr[i + 1].matches("[0-9]")){
                     String str = dartStr[i] + dartStr[i + 1];
-                    stack.add(Integer.parseInt(str));
+                    list.add(Integer.parseInt(str));
                     i += 1;
                 }else {
-                    stack.add(Integer.parseInt(dartStr[i]));
+                    list.add(Integer.parseInt(dartStr[i]));
                 }
             }else {
                 String ch = dartStr[i];
-                int num = stack.pop();
+                int num = list.get(list.size() - 1);
                 if(ch.equals("S") || ch.equals("D") || ch.equals("T")){
                     num = (int) Math.pow(num, map.get(ch));
                 }else if (dartStr[i].equals("*")) {
                     num *= 2;
-                    if (stack.size() >= 1) {
-                        int n = stack.pop();
+                    if (list.size() > 1) {
+                        int n = list.get(list.size() - 2);
                         n *= 2;
-                        stack.add(n);
+                        list.set(list.size() - 2, n);
                     }
                 }else if(dartStr[i].equals("#")) {
                     num *= -1;
                 }
-                stack.add(num);
+                list.set(list.size() - 1, num);
             }
         }
 
         int answer = 0;
-        while (!stack.isEmpty()) {
-            answer += stack.pop();
+        for (int i = 0; i < list.size(); i++) {
+            answer += list.get(i);
         }
         
         return answer;
