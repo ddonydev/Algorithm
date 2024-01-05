fcb931be0d511601a62523f18e65a33230833d20
soeun <ddonydev@gmail.com>
2024-01-05 20:21:30 +0900

    [level 1] Title: 문자열 나누기, Time: 6.18 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108#) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 9.12 ms
+메모리: 77.1 MB, 시간: 6.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 19:58:45
+2024년 1월 5일 20:21:21
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -19,9 +19,7 @@ class Solution {
                 answer++;
                 x_cnt = 0;
                 y_cnt = 0;
-                if(i + 1 == str.length){
-                    continue;
-                }else{
+                if(i + 1 != str.length){
                     x = str[i + 1];
                 }
             } else if (x_cnt != y_cnt && i == str.length - 1) {
