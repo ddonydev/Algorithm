d62b00f4c9ecc5354a9924e60ab326caae377d80
soeun <ddonydev@gmail.com>
2024-01-10 20:52:26 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.63 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,45 +1,31 @@
 import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        Map<Integer, Integer> choiceMap = new HashMap<>();
-        for(int i = 1; i <= 7; i++){
-            if(i <= 3){
-                choiceMap.put(i, 4 - i);
-            }else if(i == 4){
-                choiceMap.put(i, 0);
-            }else{
-                choiceMap.put(i, i - 4);
-            }
-        }
-
-        Map<String, Integer> type = new LinkedHashMap<>();
-        type.put("R", 0);
-        type.put("T", 0);
-        type.put("C", 0);
-        type.put("F", 0);
-        type.put("J", 0);
-        type.put("M", 0);
-        type.put("A", 0);
-        type.put("N", 0);
+        Map<String, Integer> type = new HashMap<>();
+        type.put("RT", 0);
+        type.put("CF", 0);
+        type.put("JM", 0);
+        type.put("AN", 0);
 
         for(int i = 0; i < choices.length; i++){
-            String[] str = survey[i].split("");
-            if(choices[i] <= 3){
-                type.put(str[0], type.get(str[0]) + choiceMap.get(choices[i]));
-            }else if(choices[i] > 3){
-                type.put(str[1], type.get(str[1]) + choiceMap.get(choices[i]));
+            if (type.containsKey(survey[i])) {
+                type.put(survey[i], type.get(survey[i]) + (choices[i] - 4));
+            }else{
+                survey[i] = String.valueOf(survey[i].charAt(1)) + String.valueOf(survey[i].charAt(0));
+                type.put(survey[i], type.get(survey[i]) - (choices[i] - 4));
             }
         }
-        String[][] types = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}};
+
         String answer = "";
-        for (int i = 0; i < types.length; i++) {
-            if (type.get(types[i][0]) > type.get(types[i][1])) {
-                answer += types[i][0];
-            }else if(type.get(types[i][0]) < type.get(types[i][1])){
-                answer += types[i][1];
+
+        for (String key : type.keySet()) {
+            String[] str = key.split("");
+            if (type.get(key) < 0) {
+                answer += str[0];
+            } else if (type.get(key) > 0) {
+                answer += str[1];
             }else{
-                Arrays.sort(types[i]);
-                answer += types[i][0];
+                answer += str[0];
             }
         }
         return answer;
