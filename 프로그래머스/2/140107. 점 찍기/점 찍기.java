14c6532453bd72e1940a5f2ce9f8fc094ddaa4e8
soeun <ddonydev@gmail.com>
2024-01-30 20:59:32 +0900

    [level 2] Title: 점 찍기, Time: 0.20 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+
+        for (int i = 0; i <= d; i+=k) {
+            int result = (int)Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2));
+            answer += (result / k) + 1;
+        }
+        return answer;
+    }
+}
