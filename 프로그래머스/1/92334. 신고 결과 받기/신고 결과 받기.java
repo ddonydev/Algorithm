cd839fdb9326551e9b6b1432f1be99e474a9386a
Soeun <ddonydev@gmail.com>
2024-02-23 14:02:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.20 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        Map<String, Integer> id = new HashMap<>();
+        Map<String, Set<String>> map = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+            id.put(id_list[i], i);
+            map.put(id_list[i], new HashSet<>());
+        }
+
+        for (String s : report) {
+            String[] str = s.split(" ");
+            map.get(str[1]).add(str[0]);
+        }
+
+        int[] answer = new int[id_list.length];
+        for (String s : id.keySet()) {
+            Set<String> set = map.get(s);
+            if (set.size() >= k) {
+                for (String name : set) {
+                    answer[id.get(name)]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
