1436dfe80e40619192cb7f8545cf1b389513fb42
Soeun <ddonydev@gmail.com>
2024-03-01 21:51:04 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.73 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,27 +1,24 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         map.put("code", 0);
         map.put("date", 1);
         map.put("maximum", 2);
         map.put("remain", 3);
 
-        List<int[]> list = new ArrayList<>();
-
-        for(int i = 0; i < data.length; i++){
-            if(data[i][map.get(ext)] < val_ext){
-                list.add(data[i]);
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int[] d : data) {
+            if (d[map.get(ext)] < val_ext) {
+                list.add(d);
             }
         }
 
-        Collections.sort(list, (o1, o2)->{
-            return o1[map.get(sort_by)] - o2[map.get(sort_by)];
-        });
+        Collections.sort(list, (o1, o2)
+                -> o1[map.get(sort_by)] - o2[map.get(sort_by)]);
 
-
-        int[][] answer = new int[list.size()][];
-
+        
+        int[][] answer = new int[list.size()][4];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
