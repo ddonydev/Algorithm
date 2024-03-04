d70aaeb48eeddd6ac3d1e0f934394a6465045730
Soeun <ddonydev@gmail.com>
2024-03-04 14:52:17 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int money) {
+        
+        int price = 5500;
+        
+        int[] answer = {money / price, money % price};
+        
+        return answer;
+    }
+}
