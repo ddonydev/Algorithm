e008664f00deb35cb9829236ee9d126b801a9ae5
Soeun <ddonydev@gmail.com>
2024-03-23 13:18:08 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.06 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        String upperMyString = myString.toUpperCase();
+        String upperPat = pat.toUpperCase();
+        
+        return upperMyString.contains(upperPat) ? 1 : 0;
+    }
+}
