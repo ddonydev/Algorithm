4219642d541a222e826ca6afb335094c43c67ec2
Soeun <ddonydev@gmail.com>
2024-03-25 11:21:28 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.36 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,17 +1,8 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[5];
-        int tmp = 0;
-        
-        for(int i = 0; i < num_list.length; i++){
-            for(int j = i + 1; j < num_list.length; j++){
-                if(num_list[i] > num_list[j]){
-                    tmp = num_list[i];
-                    num_list[i] = num_list[j];
-                    num_list[j] = tmp;
-                }
-            }
-        }
+        Arrays.sort(num_list);
         
         for(int i = 0; i < answer.length; i++){
             answer[i] = num_list[i];
