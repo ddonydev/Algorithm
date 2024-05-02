5ff89142c250122f95f360c8e618ca5d97608331
Soeun <ddonydev@gmail.com>
2024-05-02 23:02:51 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int num, int k) {
-        String tmp = Integer.toString(num);
-        String kNum = Integer.toString(k);
-        
-        return tmp.contains(kNum) ? tmp.indexOf(kNum) + 1 : tmp.indexOf(kNum);
+        return Integer.toString(num).contains(Integer.toString(k)) 
+            ? Integer.toString(num).indexOf(Integer.toString(k)) + 1 
+            : Integer.toString(num).indexOf(Integer.toString(k));
     }
 }
