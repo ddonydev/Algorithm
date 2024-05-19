04582ca689a48f46c5168e15c21c8971419dc181
Soeun <ddonydev@gmail.com>
2024-05-19 20:14:25 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 18.43 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char s1 = my_string.charAt(num1);
+        char s2 = my_string.charAt(num2);
+
+        my_string = my_string.substring(0, num1) + s2 + my_string.substring(num1 + 1);
+        my_string = my_string.substring(0, num2) + s1 + my_string.substring(num2 + 1);
+        return my_string;
+    }
+}
