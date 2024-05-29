77ea58426dd1629a08cda2eaa37c0932976428bc
Soeun <ddonydev@gmail.com>
2024-05-29 22:29:20 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(String number) {
-        int tmp = 0;
+        int answer = 0;
         
         for(int i = 0; i < number.length(); i++){
-            tmp += number.charAt(i) - '0';
+            answer += number.charAt(i) - '0';
         }
         
-        return tmp % 9; 
+        return answer % 9;
     }
 }
