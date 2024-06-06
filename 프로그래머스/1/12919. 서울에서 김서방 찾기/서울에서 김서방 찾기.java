336efaf8a9a8bdcff4fa2fed3a63902eefb674bd
Soeun <ddonydev@gmail.com>
2024-06-06 10:57:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.66 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public String solution(String[] seoul) {
-        int index = Arrays.asList(seoul).indexOf("Kim");
-
-        return "김서방은 " + index + "에 있다";
+        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
     }
 }
