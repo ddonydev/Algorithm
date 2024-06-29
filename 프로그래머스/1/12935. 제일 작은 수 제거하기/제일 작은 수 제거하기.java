ffbbb9ad315e359a85a16bcf8e5f2eda175acf11
Soeun <ddonydev@gmail.com>
2024-06-29 22:17:17 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.63 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,20 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        if(arr.length == 1){
-            return new int[]{-1};
-        }
+        int[] copy = arr.clone();
+        
+        Arrays.sort(copy);
         
         int[] answer = new int[arr.length - 1];
-        
-        int min = Arrays.stream(arr).min().getAsInt();
-        
         int index = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == min){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == copy[0]){
                 continue;
             }
             answer[index++] = arr[i];
+        }
+        if(answer.length == 0){
+            return new int[]{-1};
         }
         
         return answer;
