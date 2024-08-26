740bf5c70eccd3925c25c7e647b547d21f4e6634
Soeun <ddonydev@gmail.com>
2024-08-26 19:17:17 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,17 +1,13 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         for(int i = 0; i < finished.length; i++){
             if(!finished[i]){
-                list.add(i);
+                list.add(todo_list[i]);
             }
         }
         
-        String[] answer = new String[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = todo_list[list.get(i)];
-        }
-        return answer;
+        return list.toArray(new String[0]);
     }
 }
