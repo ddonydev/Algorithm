07d0cde193526846d14519c89808b2aa7e0f58ce
Soeun <ddonydev@gmail.com>
2024-09-11 19:28:13 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -3,15 +3,18 @@ class Solution {
         int x = dot[0];
         int y = dot[1];
         
-            if(x > 0 && y > 0){
-                return 1;
-            }else if(x < 0 && y > 0){
-                return 2;
-            }else if(x < 0 && y < 0){
-                return 3;
-            }else if(x > 0 && y < 0){
-                return 4;
+            if(x > 0){
+                if(y > 0){
+                    return 1;
+                }else{
+                    return 4;
+                }
+            }else{
+                if(y > 0){
+                    return 2;
+                }else{
+                    return 3;
+                }
             }
-        return 0;
     }
 }
