b758e958a368f0f1b2219eaf55cbda00e3311b8e
Soeun <ddonydev@gmail.com>
2024-09-14 15:28:56 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.09 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        
         if((a % 2 != 0) && (b % 2 != 0)){
             return (a * a) + (b * b);
         }else if((a % 2 != 0) || b % 2 != 0){
