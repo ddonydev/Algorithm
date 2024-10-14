1389f41e2c48618616049edcaa4487b5cdeb3bea
Soeun <ddonydev@gmail.com>
2024-10-14 16:49:38 +0900

    [level 0] Title: 문자열 섞기, Time: 0.22 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
-        
+        StringBuffer sb = new StringBuffer();
         for(int i = 0; i < str1.length(); i++){
-            answer += String.valueOf(str1.charAt(i)) + String.valueOf(str2.charAt(i));
+            sb.append(String.valueOf(str1.charAt(i)));
+            sb.append(String.valueOf(str2.charAt(i)));
         }
         
-        return answer;
+        return sb.toString();
     }
 }
