e23778f9ec336c6b6b08d9ce0899c80918fbfce1
Soeun <ddonydev@gmail.com>
2024-10-20 21:17:43 +0900

    [level 0] Title: ad 제거하기, Time: 0.83 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] strArr) {
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
         for (int i = 0; i < strArr.length; i++) {
             if(!strArr[i].contains("ad")){
