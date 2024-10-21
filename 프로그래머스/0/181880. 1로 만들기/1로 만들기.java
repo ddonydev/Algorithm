aba675ae650954d49c221e3cb2859c9c51dc3f11
Soeun <ddonydev@gmail.com>
2024-10-21 18:43:09 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        
-        
-        for(int i = 0; i < num_list.length; i++){
+        int i = 0;
+        while(i < num_list.length){
             while(num_list[i] != 1){
                if(num_list[i] % 2 != 0){
                    num_list[i] -= 1;
@@ -11,7 +10,7 @@ class Solution {
                 num_list[i] /= 2;
                 answer++;
             }
-
+            i++;
         }
         return answer;
     }
