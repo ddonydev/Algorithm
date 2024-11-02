d57d1fbf4aa04b6cb6f2d73972eb1cd558cf90aa
Soeun <ddonydev@gmail.com>
2024-11-02 11:39:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean solution(int x) {
+        String strNum = Integer.toString(x);
+        int[] arrNum = new int[strNum.length()];
+        for (int i = 0; i < strNum.length(); i++) {
+            arrNum[i] = strNum.charAt(i) - '0';
+        }
+
+        int num = 0;
+        for (int i = 0; i < arrNum.length; i++) {
+            num += arrNum[i];
+        }
+        if((x % num) == 0) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
