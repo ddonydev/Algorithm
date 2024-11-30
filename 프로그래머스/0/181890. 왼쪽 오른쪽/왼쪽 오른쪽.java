ff4320f03f52591496cca69fe4da78d188547a3c
Soeun <ddonydev@gmail.com>
2024-11-30 12:51:58 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.10 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+
+        ArrayList<String> str = new ArrayList<>(Arrays.asList(str_list));
+        ArrayList<String> list = new ArrayList<>();
+
+        if(str.contains("l") && str.contains("r")){
+            int l = str.indexOf("l");
+            int r = str.indexOf("r");
+
+            if(l < r){
+                for (int i = 0; i < l; i++) {
+                    list.add(str.get(i));
+                }
+            }else{
+                for (int i = r + 1; i < str.size(); i++) {
+                    list.add(str.get(i));
+                }
+            }
+        }else if(str.contains("l") && !str.contains("r")){
+            int l = str.indexOf("l");
+            for (int i = 0; i < l; i++) {
+                list.add(str.get(i));
+            }
+        }else if(!str.contains("l") && str.contains("r")){
+            int r = str.indexOf("r");
+            for (int i = r + 1; i < str.size(); i++) {
+                list.add(str.get(i));
+            }
+        }else{
+            list.add("");
+        }
+
+        answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) == ""){
+                answer = new String[]{};
+                break;
+            }
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
