b601f4f0e678cf10ce35861996239e36c68eb098
Soeun <ddonydev@gmail.com>
2024-12-07 17:52:12 +0900

    [level 1] Title: 명예의 전당 (1), Time: 8.34 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
 
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < score.length; i++) {
             list.add(score[i]);
