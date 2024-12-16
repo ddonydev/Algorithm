5c17becfeb3db7c67e6d7c09db20edbebc8a8843
Soeun <ddonydev@gmail.com>
2024-12-16 13:23:19 +0900

    [level 2] Title: 튜플, Time: 196.16 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -10,7 +10,7 @@ class Solution {
                 map.put(Integer.valueOf(str[i]), map.getOrDefault(Integer.valueOf(str[i]), 0) + 1);
             }
         }
-        List<Integer> keySetList = new ArrayList<>(map.keySet());
+        ArrayList<Integer> keySetList = new ArrayList<>(map.keySet());
         Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
 
         int[] answer = new int[map.size()];
