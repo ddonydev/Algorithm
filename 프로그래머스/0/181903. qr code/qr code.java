6580e2f021a3d5b899a677dee195b393b94119a2
Soeun <ddonydev@gmail.com>
2024-12-21 19:45:36 +0900

    [level 0] Title: qr code, Time: 0.21 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,13 +1,12 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        String answer = "";
-        
+        StringBuffer sb = new StringBuffer();
         for(int i = 0; i < code.length(); i++){
             if(i % q == r){
-                answer += code.charAt(i);
+                sb.append(code.charAt(i));
             }
         }
         
-        return answer;
+        return sb.toString();
     }
 }
