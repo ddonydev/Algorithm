1dc739f3dcacc4e591d61c20b905a978bf6be237
Soeun <ddonydev@gmail.com>
2024-12-24 17:12:33 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.22 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,18 +1,15 @@
+import java.util.*;
 class Solution {
     public int solution(String binomial) {
         String[] str = binomial.split(" ");
         int num1 = Integer.parseInt(str[0]);
         int num2 = Integer.parseInt(str[2]);
         
-        switch(str[1]){
-            case"+":
-                return num1 + num2;
-            case"-":
-                return num1 - num2;
-            case"*":
-                return num1 * num2;
-            default:
-                return 0;
-        }
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("+", num1 + num2);
+        map.put("-", num1 - num2);
+        map.put("*", num1 * num2);
+        
+        return map.get(str[1]);
     }
 }
