734b5a65a54e693ba2f626dd1b95392e312a21d0
Soeun <ddonydev@gmail.com>
2024-12-25 15:05:12 +0900

    [level 0] Title: 세 개의 구분자, Time: 75.05 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,22 +3,17 @@ class Solution {
     public String[] solution(String myStr) {
         String[] str = myStr.split("[abc]");
 
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < str.length; i++) {
             if(str[i] != null && !str[i].equals("")){
                 list.add(str[i]);
             }
         }
-        String[] answer = new String[list.size()];
-
+        
         if(list.size() > 0){
-            for (int i = 0; i < list.size(); i++) {
-                answer[i] = list.get(i);
-            }
+            return list.toArray(String[]::new);
         }else{
-            answer = new String[1];
-            answer[0] = "EMPTY";
+            return new String[]{"EMPTY"};
         }
-        return answer;
     }
 }
