0fb13b11286fb66d81a9f2e54d08cc0f02f0f983
Soeun <ddonydev@gmail.com>
2024-12-28 16:28:00 +0900

    [level 0] Title: 세균 증식, Time: 0.10 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-
-        for(int i = 0; i < t + 1; i++){
-           answer = n * (int) Math.pow(2, i);
-        }
-        
-        return answer;
+        return n * (int) Math.pow(2, t);
     }
 }
